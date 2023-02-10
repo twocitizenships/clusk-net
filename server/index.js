9ce88a89fee764a6f700ec9c0c8ca1334356b371
clusk <clusk@strike.me>
2023-02-09 19:48:04 -0600

    Change build start

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../html/index.html')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" })
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../html/', 'index.html'));
   });
 
 app.listen(PORT, () => {
