a55d3a493b7f06302872a409d331f2a5eb26bcb8
clusk <clusk@strike.me>
2023-02-09 19:49:15 -0600

    Change resolution

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" })
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../html/', 'index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../html/', 'index.html'));
+//   });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
