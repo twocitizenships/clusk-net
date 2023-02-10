015945f16dcb7ebefb064c872f2ff09f8eb1762e
clusk <clusk@strike.me>
2023-02-10 00:14:44 -0600

    Image references

--- server/index.js
+++ server/index.js
@@ -26,8 +26,52 @@ app.get("/images/BlogBanner-min.png", (req, res) => {
     res.sendFile(path.resolve('../client/public/html/images/', 'BlogBanner-min.png'));
 });
 
-app.get("/images/AccordionSpain.jpeg", (req, res) => {
-    res.sendFile(path.resolve('../client/public/html/images/film/', 'AccordionSpain.jpeg'));
+app.get("/images/BlogBanner-min.png", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/', 'BlogBanner-min.png'));
+});
+
+app.get("/images/spotify.svg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'spotify.svg'));
+});
+
+app.get("/images/github.svg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'github.svg'));
+});
+
+app.get("/images/AirplaneWingSpain.jpeg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'AirplaneWingSpain.jpeg'));
+});
+
+app.get("/images/BullFightSpain.jpeg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'BullFightSpain.jpeg'));
+});
+
+app.get("/images/CookingKuwait.jpeg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'CookingKuwait.jpeg'));
+});
+
+app.get("/images/EdgeOfWorldAtami.jpg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'EdgeOfWorldAtami.jpg'));
+});
+
+app.get("/images/HeavenAtami.jpg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'HeavenAtami.jpg'));
+});
+
+app.get("/images/Paradise2Atami.jpg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'Paradise2Atami.jpg'));
+});
+
+app.get("/images/SheepKuwait.jpeg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'SheepKuwait.jpeg'));
+});
+
+app.get("/images/SlimAaronsAtami.jpg", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/images/film/', 'SlimAarons.jpg'));
+});
+
+app.get("/styles.css", (req, res) => {
+    res.sendFile(path.resolve('../client/public/html/', 'styles.css'));
 });
 
 app.get("/film.html", (req, res) => {
