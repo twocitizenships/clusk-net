9cde3bcd9af9a07a291adf5c9f0e37b45ac401ba
clusk <clusk@strike.me>
2023-02-09 20:38:55 -0600

    Update image links

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../html')));
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" })
-});
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" })
+// });
 
 app.get("/links.html", (req, res) => {
     res.sendFile(path.resolve(__dirname, '../html/', 'links.html'));
