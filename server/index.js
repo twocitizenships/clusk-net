2716d549071f33842b7d8a4a7f5430ecd4aaed70
clusk <clusk@strike.me>
2023-02-09 21:00:17 -0600

    Move files

--- server/index.js
+++ server/index.js
@@ -3,26 +3,26 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../html')));
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // app.get("/api", (req, res) => {
 //     res.json({ message: "Hello from server!" })
 // });
 
 app.get("/links.html", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../html/', 'links.html'));
+    res.sendFile(path.resolve(__dirname, '../public/', 'links.html'));
 });
 
 app.get("/film.html", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../html/', 'film.html'));
+    res.sendFile(path.resolve(__dirname, '../public/', 'film.html'));
 });
 
 app.get("/blog.html", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../html/', 'blog.html'));
+    res.sendFile(path.resolve(__dirname, '../public/', 'blog.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../html/', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../public/', 'index.html'));
 });
 
 app.listen(PORT, () => {
