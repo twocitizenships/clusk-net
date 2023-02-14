bcf43e390e33badd10e73a33f731b2205088a3dc
clusk <clusk@strike.me>
2023-02-14 11:26:24 -0600

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Image from 'next/image'
-import Links from './links'
 import Header from './header'
 import Footer from './footer'
 import Bio from './bio'
@@ -13,13 +11,10 @@ export default function Home() {
         <Link rel="stylesheet" href="../styles/globals.css" />
         <Link rel="icon" href="/favicon.ico" />
         <Link href="https://fonts.cdnfonts.com/css/menlo" rel="stylesheet" />
-        <title>Clusk.net | Photographer, Writer, Engineer</title>
       </Head>
       <main>
-        {/* Convert these to links */}
         <Header />
         <Bio />
-        {/* <!-- footer --> */}
         <Footer />
       </main>
     </>
