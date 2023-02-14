be3ac5966266b2384a7d05451c28c7562bacc818
clusk <clusk@strike.me>
2023-02-14 12:35:54 -0600

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '1', // Or a number of instances
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'deploy:prod',
       env_local: {
         APP_ENV: 'local' // APP_ENV=local
       },
