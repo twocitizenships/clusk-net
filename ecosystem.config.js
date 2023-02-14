7ca3b9346439d5f316cdbdba7afb06653f162eff
clusk <clusk@strike.me>
2023-02-14 11:53:07 -0600

    Update Ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  apps : [{
-    name   : "app1",
-    script : "./app.js"
-  }]
+  apps: [
+    {
+      name: 'clusk-net',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env_local: {
+        APP_ENV: 'local' // APP_ENV=local
+      },
+      env_development: {
+        APP_ENV: 'dev' // APP_ENV=dev
+      },
+      env_production: {
+        APP_ENV: 'prod' // APP_ENV=prod
+      }
+    }
+  ]
 }
