38f4798cd479fada685bbf3e86c9a3e45fce9a52
clusk <clusk@strike.me>
2023-02-15 12:49:04 -0600

    Rework header

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Link rel="stylesheet" href="../styles/globals.css" />
+        <Link rel="icon" href="/favicon.ico" />
+        <Link href="https://fonts.cdnfonts.com/css/menlo" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/header.js
+++ src/pages/header.js
@@ -3,7 +3,7 @@ export default function Header() {
         <div className="HeaderClass">
             <title>Clusk.net | Photographer, Writer, Engineer</title>
             <a href="/"><img src="/Home.png"></img></a>
-            <style jsx>{`padding:4px`}</style>
+            {/* <style jsx>{`padding:4px`}</style> */}
             <a href="/links"><img src="/Links.png"></img></a>
         </div>
     );
