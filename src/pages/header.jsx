e51af066d3cedcc073d0f407f46e05aff1af2dc7
clusk <clusk@strike.me>
2023-02-16 17:58:02 -0600

    Adjust image spacing

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,9 +1,8 @@
 export default function Header() {
     return (
         <div className="HeaderClass">
-            <title>Clusk.net | Photographer, Writer, Engineer</title>
             <a href="/"><img src="/Home.png"></img></a>
-            {/* <style jsx>{`padding:4px`}</style> */}
+            <style jsx>{`padding:4px`}</style>
             <a href="/links"><img src="/Links.png"></img></a>
         </div>
     );
