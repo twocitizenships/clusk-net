f0ed76a57150e5b3e363146292461b1071702771
clusk <clusk@strike.me>
2023-02-16 17:22:54 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Header from './header'
 import Footer from './footer'
 import Bio from './bio'
@@ -8,9 +7,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <Link rel="stylesheet" href="../styles/globals.css" />
-        <Link rel="icon" href="/favicon.ico" />
-        <Link href="https://fonts.cdnfonts.com/css/menlo" rel="stylesheet" />
       </Head>
       <main>
         <Header />
