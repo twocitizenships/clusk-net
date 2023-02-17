70fb40a3d135b051d5c21747cd36b519f2313732
clusk <clusk@strike.me>
2023-02-16 18:10:25 -0600

    Add custom 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function FourOhFour() {
+    return (
+        <div>
+            <Header />
+            <p><b>[404] Error</b> <br /><br />Hmm... you've gone off the reservation.  Time to go back home.</p>
+            <Footer />
+        </div>
+    );
+}
