6a6b8db093bf04dc59bd52bad51f176b46fe44fa
clusk <clusk@strike.me>
2023-02-16 22:33:27 -0600

    Pin footer to bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="FooterClass">
+        <div style={{ position: "absolute", bottom: 0, width:"100%" }} className="FooterClass">    
             <p> <style jsx>{`color:red`}</style>_______________________</p>
             <p>A personal site for clusk <a href="https://twitter.com/twocitizenships">[@twocitizenships]</a></p>
         </div>
