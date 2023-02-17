3d13bb2bbdaff1bd16270299c7f0e2748cd0ed1d
clusk <clusk@strike.me>
2023-02-16 18:03:49 -0600

    Reorganize

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Header from './header'
-import Footer from './footer'
-import Bio from './bio'
+import Header from './components/header'
+import Footer from './components/footer'
+import Bio from './components/bio'
 
 export default function Home() {
   return (
