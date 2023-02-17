384584af69ffd8afd6d4de8ca97993dabc9951d3
clusk <clusk@strike.me>
2023-02-16 22:40:27 -0600

    Formatting

--- src/pages/links.jsx
+++ src/pages/links.jsx
@@ -7,12 +7,10 @@ export default function Links() {
             <Header />
             <p>&#127902;: <a href="/film">@twocitizenships</a></p>
             <p><style jsx>{`height:15px`}</style>
-                <img src="/github.svg"></img>:
-                <a href="https://github.com/twocitizenships">@twocitizenships</a>
+                <img src="/github.svg"></img>: <a href="https://github.com/twocitizenships">@twocitizenships</a>
             </p>
             <p><style jsx>{`height:15px`}</style>
-                <img src="/spotify.svg"></img>: 
-                <a href="https://open.spotify.com/artist/4QyBej1bOaOJRWEDtLh8GZ?si=gP8b84x5S2K40gq2cWrlWA">realpolitik</a>
+                <img src="/spotify.svg"></img>: <a href="https://open.spotify.com/artist/4QyBej1bOaOJRWEDtLh8GZ?si=gP8b84x5S2K40gq2cWrlWA"> realpolitik</a>
             </p>
             <p>&#128231;: <a href="mailto:twocitizenships@gmail.com">twocitizenships@gmail.com</a></p>
             <Footer />
