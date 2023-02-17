694fa246e9d6fb35cbf91e7526ef388b7c2b0f0c
clusk <clusk@strike.me>
2023-02-16 21:46:31 -0600

    Add girlfriend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,8 @@ export default function Header() {
             <a href="/"><img src="/Home.png"></img></a>
             <style jsx>{`padding:4px`}</style>
             <a href="/links"><img src="/Links.png"></img></a>
+            <style jsx>{`padding:4px`}</style>
+            <a href="/girlfriend"><img src="/Gf.png"></img></a>
         </div>
     );
 }
---
+++ src/pages/girlfriend.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+
+export default function Contact() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        handle: "",
+        bmi: "",
+        nationality: "",
+        message: ""
+    });
+
+    const [formSuccess, setFormSuccess] = useState(false)
+    const [formSuccessMessage, setFormSuccessMessage] = useState("")
+
+    const handleInput = (e) => {
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;
+
+        setFormData((prevState) => ({
+            ...prevState,
+            [fieldName]: fieldValue
+        }));
+    }
+
+    const submitForm = (e) => {
+        // We don't want the page to refresh
+        e.preventDefault()
+
+        const formURL = e.target.action
+        const data = new FormData()
+
+        // Turn our formData state into data we can use with a form submission
+        Object.entries(formData).forEach(([key, value]) => {
+            data.append(key, value);
+        })
+
+        // POST the data to the URL of the form
+        fetch(formURL, {
+            method: "POST",
+            body: data,
+            headers: {
+                'accept': 'application/json',
+            },
+        }).then((response) => response.json())
+            .then((data) => {
+                setFormData({
+                    name: "",
+                    email: "",
+                    handle: "",
+                    message: ""
+                })
+
+                setFormSuccess(true)
+                setFormSuccessMessage(data.submission_text)
+            })
+    }
+
+    return (
+        <div>
+            <Header />
+            <form action="/api/form" method="post">
+                <br />
+                <div><label htmlFor="twitter">Twitter: </label>
+                    <input type="text" id="twitter" name="twitter" required />
+                </div>
+                <br />
+                <div><label htmlFor="age">Age: </label>
+                    <input type="number" id="age" name="age" maxLength="2" max="25" required />
+                </div>
+                <br />
+                <div><label htmlFor="bmi">Body Mass Index [BMI]: </label>
+                    <input type="number" id="bmi" name="bmi" maxLength="2" max="25" required />
+                </div>
+                <br />
+                <div><label htmlFor="nationality">Nationality: </label>
+                    <input type="text" id="nationality" name="nationality" required />
+                </div>
+                <br />
+                <div><label htmlFor="message">Impress me: </label>
+                    <textarea id="message" name="message"/>
+                </div>
+                <br />
+                <div><label htmlFor="message">Image Upload: [coming soon!] </label>
+                </div>
+                <br />
+                <button type="submit">Send Girlfriend Application</button>
+            </form>
+            <Footer />
+        </div>
+    )
+}
