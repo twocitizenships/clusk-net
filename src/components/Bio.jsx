b94af3b4e7a8424a67c60e228ce1316347648b11
clusk <clusk@strike.me>
2023-02-25 19:53:01 -0500

    Update bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -5,7 +5,8 @@ export default function Bio() {
             <h3>&#127902; &#128747; &#128190;</h3>
             <p>Bookings &#10145; <a href="mailto:twocitizenships@gmail.com">twocitizenships@gmail.com</a></p>
             <p>Buy postcards, music and books &#10145;  <a href="https://clusk.gumroad.com">clusk.gumroad.com</a></p>
-            <p><b>&#127940; El Salvador: Until 18 Feb 2023</b></p>
+            <p><b>&#127480;&#127483; El Salvador: Until 18 Mar 2023</b></p>
+            <p><b>&#127464;&#127479; Costa Rica: 19 Mar - 22 Mar 2023</b></p>
         </div>
     )
 }
